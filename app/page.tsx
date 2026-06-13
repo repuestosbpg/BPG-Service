988d497ce4ad1bc78fdcb3da4857e728ef3c0fa5
repuestosbpg <repuestosbpg@gmail.com>
2026-06-13 02:00:06 -0400

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -4,4 +4,4 @@ export default function Home() {
 {name:"Toallitas Emubaby Water 99%",precio:"$1.500",imagen:"/emubaby.jpg"},
 {name:"Pañales Nenito Talla M",precio:"$5.000",imagen:"/nenito.jpg"}
 ];
-return (<main className="container"><header><h1>BPG Service</h1><p>Ofertas destacadas</p></header><div className="grid">{productos.map((p)=><div className="card" key={p.name}><img src={p.imagen} alt={p.nombre}/><h2>{p.nombre}</h2><p className="price">{p.precio}</p><a className="button" href={`https://wa.me/56945671898?text=${encodeURIComponent(`Hola, quiero comprar ${p.nombre}`)}`} target="_blank">Comprar por WhatsApp</a></div>)}</div></main>)}
+return (<main className="container"><header><h1>BPG Service</h1><p>Ofertas destacadas</p></header><div className="grid">{productos.map((p)=><div className="card" key={p.name}><img src={p.imagen} alt={p.nombre}/><h2>{p.nombre}</h2><p className="price">{p.precio}</p><a className="button" href={`https://wa.me/56945671898?text=${encodeURIComponent(`Hola, quiero comprar ${p.name}`)}`} target="_blank">Comprar por WhatsApp</a></div>)}</div></main>)}
